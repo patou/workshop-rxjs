03f2f28b5f71c07efd770514dd7feea794d36398
Patrice De Saint Steban <patrice@desaintsteban.fr>
2020-09-23 16:21:57 +0000

    WIP

--- excercice/excercice1.js
+++ excercice/excercice1.js
@@ -1,6 +1,10 @@
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map, filter } from 'rxjs/operators'
 
 console.log('test');
 of(1, 2, 3, 4, 5)
-.pipe(map(console.log)).subscribe();
+                  .pipe(
+                      filter(x => x % 2 === 0),
+                      map(x => x + 1),
+                  )
+                  .subscribe(console.log);
